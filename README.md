a4bd7496a2a3e4712bde7d0d65636ba323441fe9
Mehmet Eker <mehmeteker@192.168.1.13>
2024-07-26 02:06:44 +0300

    Update

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Convert Project
+# YOLO Microservice Project
+
+This project provides a microservice that uses the YOLO model to detect objects in images. It offers a Flask-based REST API and processes image files, returning detected objects in JSON format.
+
+## Requirements
+
+To run this project, you need to install the following dependencies:
+
+```bash
+    pip install -r requirements.txt
+```
+# Installation and Usage
+1.	Clone the project:
+
+```bash
+    git clone https://github.com/MehmetEker1/yolo-to-onnx
+    cd yolo-microservice-project/convert
+```
+2.	Create and activate a virtual environment:
+```
+    python -m venv venv
+    source venv/bin/activate  # macOS or Linux
+    venv\Scripts\activate  # Windows
+```
+3.	Install the required dependencies:
+
+```bash 
+    pip install -r requirements.txt
+```
+4. Convert the YOLO model to ONNX format:
+```bash
+    python yolo_to_onnx.py
+```
+
+You have completed setting up and using the YOLO to ONNX Conversion Project. If you have any questions or encounter issues, please feel free to open an issue in the project repository.
--- yolo_to_onnx.py
+++ yolo_to_onnx.py
@@ -4,34 +4,34 @@ import onnx
 import onnxruntime as ort
 import numpy as np
 
-# YOLOv5m modelini indir ve yükle
+# Download the YOLOv5m model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5m')
 
-# Modeli değerlendirme moduna al
+# Set the model to evaluation mode
 model.eval()
 
-# Örnek bir giriş tensörü (modelin beklediği boyutlarda)
+# An example input tensor (with dimensions expected by the model)
 dummy_input = torch.randn(1, 3, 640, 640)
 
-# Modeli ONNX formatına dönüştür
+# Convert the model to ONNX format
 onnx_model_path = "yolov5m.onnx"
 torch.onnx.export(model, dummy_input, onnx_model_path, verbose=True, opset_version=11)
 
-# ONNX modelini yükle
+# Load the ONNX model
 onnx_model = onnx.load(onnx_model_path)
 
-# Modeli doğrula
+# Check model
 onnx.checker.check_model(onnx_model)
 
-# ONNX Runtime ile model oturumunu oluştur
+# Create a model session with ONNX Runtime
 ort_session = ort.InferenceSession(onnx_model_path)
 
-# Modelin beklediği girdi adlarını al
+#  Get the input names expected by the model
 input_names = [input.name for input in ort_session.get_inputs()]
 print("Model Inputs:", input_names)
 
-# Aynı giriş tensörünü kullanarak tahmin yap
+#make a prediction using the same input tensor
 outputs = ort_session.run(None, {input_names[0]: dummy_input.numpy()})
 
-# Çıktıları inceleyin
+# Examine the outputs
 print(outputs)
